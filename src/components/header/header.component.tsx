ea63c2f4c68a5ac816d4b43e0a7efe65a746b48d
root <root@DESKTOP-5N4FH3C.localdomain>
2023-03-20 02:12:27 +0330

    cleanup

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,28 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { useMediaQuery } from "@mui/material";
-import dynamic from "next/dynamic";
 
-const BigSizeHeader = dynamic(() => import("./big-size"), {
-    loading: () => <div>loading ...</div>
-});
-const SmallSizeHeader = dynamic(() => import("./small-size"), {
-    loading: () => <div>loading ...</div>
-});
+
+const BigSizeHeader = React.lazy(() => import("./big-size"));
+const SmallSizeHeader = React.lazy(() => import("./small-size"));
 
 
 const Header: React.FC<{}> = () => {
-    const [show, setShow] = useState(true);
+    const [show, setShow] = useState(false);
     const match = useMediaQuery("(min-width:750px)");
+
 
     // this useEffect its not useless the reason i have to use useeffect 
     // is because this issue (https://github.com/mui/material-ui/issues/21142) which comes from useMediaQuery()
     // in server side rendered applications
-    // useEffect(() => {
-    //     setShow(true)
-    // }, [])
+    useEffect(() => {
+        setShow(true)
+    }, [])
+
+
     return (
         <>
-            {show && (!match ? <SmallSizeHeader /> : <BigSizeHeader />)}
+            {
+                show && (
+                    match
+                        ?
+                        <Suspense fallback={<div>loading</div>}>
+                            <BigSizeHeader />
+                        </Suspense>
+                        :
+                        <Suspense
+                            fallback={<div>loading</div>}
+                        >
+                            <SmallSizeHeader />
+                        </Suspense>
+                )
+            }
         </>
     )
 }
