6a185bec6cddfdc3348bbcf7f4a321c3f877424b
amirreza-ashrafi-dev <aa090818@yahoo.com>
2023-04-22 20:58:11 +0430

    mobile header animated

--- src/components/header/small-size.tsx
+++ src/components/header/small-size.tsx
@@ -9,9 +9,20 @@ import Logo from "@/icons/logo/logo.component";
 
 const MobileHeader: React.FC<{}> = () => {
     const [showMenu, setShowMenu] = useState(false);
+    const MenuAnimation = {
+        initial: { opacity: 0, height: 0 },
+        animation: { opacity: 1, height: "auto" }
+    }
 
     return (
-        <Box padding="15px 10px" display={"flex"} flexDirection={"column"}>
+        <Box
+            padding="15px 10px"
+            display={"flex"}
+            flexDirection={"column"}
+            left={0}
+            right={0}
+            zIndex={2000}
+        >
             <Box display={"flex"} justifyContent="space-between">
                 <Box>
                     <Logo />
@@ -19,35 +30,42 @@ const MobileHeader: React.FC<{}> = () => {
                 <MobileToggleBtn Click={() => setShowMenu(!showMenu)} Show={showMenu} />
             </Box>
             <AnimatePresence>
-                {showMenu && <>
-                    <motion.div initial={{ y: -210, opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ y: -210, opacity: 0 }}>
-                        <Box display={"flex"} flexDirection="column" justifyContent="center" alignItems="center">
-                            <Image style={{ marginRight: "10px" }} width={70} src={ProfileIcon} alt="Profile pic" />
-                            <Box margin={"10px 0 5px 0"}>
-                                <Typography variant="body1" fontSize={"14px"} fontFamily={"Monteserrat"}>
-                                    John D.
-                                </Typography>
-                            </Box>
+                <motion.div
+                    initial={MenuAnimation.initial}
+                    transition={{ type: 'spring', damping: 25 }}
+                    animate={
+                        showMenu
+                            ?
+                            MenuAnimation.animation
+                            :
+                            MenuAnimation.initial
+                    }
+                >
+                    <Box display={"flex"} flexDirection="column" justifyContent="center" alignItems="center">
+                        <Image style={{ marginRight: "10px" }} width={70} src={ProfileIcon} alt="Profile pic" />
+                        <Box margin={"10px 0 5px 0"}>
+                            <Typography variant="body1" fontSize={"14px"} fontFamily={"Monteserrat"}>
+                                John D.
+                            </Typography>
                         </Box>
-                        <Box display={"flex"} justifyContent="center" alignItems="center">
-                            <Box style={{ width: "fit-content" }}>
-                                <Typography variant="body1" fontSize={"14px"} fontFamily={"Monteserrat"}>
-                                    Favourites
-                                </Typography>
-                                <Typography variant="body1" fontSize={"14px"} fontFamily={"Monteserrat"}>
-                                    Find Flight
-                                </Typography>
-                                <Typography variant="body1" fontSize={"14px"} fontFamily={"Monteserrat"}>
-                                    Find Stays
-                                </Typography>
-                            </Box>
+                    </Box>
+                    <Box display={"flex"} justifyContent="center" alignItems="center">
+                        <Box style={{ width: "fit-content" }}>
+                            <Typography variant="body1" fontSize={"14px"} fontFamily={"Monteserrat"}>
+                                Favourites
+                            </Typography>
+                            <Typography variant="body1" fontSize={"14px"} fontFamily={"Monteserrat"}>
+                                Find Flight
+                            </Typography>
+                            <Typography variant="body1" fontSize={"14px"} fontFamily={"Monteserrat"}>
+                                Find Stays
+                            </Typography>
+                        </Box>
 
-                        </Box>
-                    </motion.div>
-                </>}
+                    </Box>
+                </motion.div>
             </AnimatePresence>
         </Box>
-
     )
 }
 
